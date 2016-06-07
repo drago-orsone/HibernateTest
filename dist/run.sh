58d4da956fbd05a0e17a9d58d82d3c7d2000bbef
drago-orsone <alberto_dragoni@hotmail.com>
2016-06-07 16:58:18 +0200

    Ivy and dist upgrade

---
+++ dist/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+main=HibernateTest
+#logConfig=cfg/log4j2.xml
+
+#java -jar -Dlog4j.configurationFile=$logConfig bin/$main.jar $*
+java -jar  bin/$main.jar $*
---
+++ src/it/csttech/hibernatetest/drago_orsone/Test.java
@@ -0,0 +1,17 @@
+package it.csttech.hibernatetest.drago_orsone;
+
+import org.hibernate.SessionFactory;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Faccio nulla");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Test();
+	}
+
+}
